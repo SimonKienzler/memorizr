44be5d003bfa4c548945d58fe58bc6700b1ad6e5
Simon Kienzler <simon.kienzler@gmx.de>
2017-09-17 15:39:43 +0200

    Established basic app layout

--- memorizr/gui/renderer.php
+++ memorizr/gui/renderer.php
@@ -3,11 +3,38 @@
 	class Renderer {
 	
 		public static function error($heading, $logMessage) {
-			return "<p><span class='error'>" . $heading . "</span><br />" . $logMessage . "</p>";
+			return "<h2><span class='error'>" . $heading . "</span></h2><p>" . $logMessage . "</p>";
 		}
 		
 		public static function info($heading, $logMessage) {
-			return "<p><span class='info'>" . $heading . "</span><br />" . $logMessage . "</p>";
+			return "<h2><span class='info'>" . $heading . "</span></h2><p>" . $logMessage . "</p>";
+		}
+		
+		public static function top() {
+			return "<div id='top'><h1>Mem<span class='fa fa-play-circle'></span>rizr</h1></div>";
+		}
+		
+		public static function navigation() {
+			$pages = array(
+				"index" => array("index","Suche","search"),
+				"import" => array("import","Importieren","upload"),
+				"playlists" => array("playlists","Playlisten","bars"),
+				"memory" => array("memory","Memorize me","star")
+			);
+			
+			$navString ="<ul id='navi'>";
+			
+			foreach($pages as $page) {
+				$navString = $navString . "<li><a href='?page=" . $page[0] . 
+										"'><span class=' fa fa-" . $page[2] . 
+										"'></span>" . $page[1] . "</a></li>";
+			}
+			
+			return $navString . "</ul>";
+		}
+		
+		public static function player() {
+			return "<div id='player'></div>";
 		}
 	
 	}
--- memorizr/gui/web.php
+++ memorizr/gui/web.php
@@ -2,13 +2,15 @@
 <html>
   <head>
     <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title><?php echo "Memorizr | Memory Music Player"; ?></title>
-  <link rel="stylesheet" href="./memorizr/gui/main.css" type="text/css">
+    <link rel="stylesheet" href="./memorizr/gui/main.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
   </head>
   <body>
 	<?php 
 		require("/../app.php");
+		require_once("renderer.php");
 	
 		$memorizr = new Memorizr();
 		
@@ -16,9 +18,18 @@
 			$page = $_GET['page'];
 		} else {
 			$page = "index";
-		} 
+		}
 		
+		echo Renderer::top();	
+		echo "<div id='main'>";
+		echo Renderer::navigation();
+		echo "<div id='memo-output'>";
 		$memorizr->run($page);
+		echo "</div>";
+		echo Renderer::player();
+		echo "</div>";
+		
+		
 	?>
   </body>
 </html>
